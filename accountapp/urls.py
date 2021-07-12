ec8eaffbe2f6199426a5ff51568cc73841fa46b5
xxoellie <elliesohyeon1202@gmail.com>
2021-07-12 15:15:53 +0900

    xxoellie practice 7/12 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from accountapp.views import hello_world
 
-appname = 'accountapp'
+app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello world')
+    path('hello_world/', hello_world, name='hello_world')
     ]
+
