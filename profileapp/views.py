3848777874a456952825c96093c03f5f4f77ad3d
xxoellie <elliesohyeon1202@gmail.com>
2021-07-28 09:29:11 +0900

    xxoellie practice 7/28 1

--- profileapp/views.py
+++ profileapp/views.py
@@ -4,7 +4,7 @@ from profileapp.models import Profile
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 
@@ -18,3 +18,11 @@ class ProfileCreateView(CreateView) :
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ProfileUpdateView(UpdateView) :
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
+
